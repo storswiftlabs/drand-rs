d9fc11b732d2c47d8728e018c497df3a3c4f4d77
version513 <88940005+version513@users.noreply.github.com>
2025-03-13 14:15:38 +0800

    dkg: add message_for_signing (#32)

--- src/dkg/actions_signing.rs
+++ src/dkg/actions_signing.rs
@@ -1,3 +1,8 @@
+use crate::transport::dkg::GossipData;
+use crate::transport::dkg::GossipPacket;
+use crate::transport::dkg::ProposalTerms;
+use crate::transport::drand::Identity;
+
 use prost_types::Timestamp;
 
 /// Implementation for UTC 0, aligned to https://pkg.go.dev/time#Time.MarshalBinary [go 1.22.10]
@@ -25,6 +30,89 @@ fn enc_timestamp(t: Timestamp) -> [u8; 15] {
         0xFF,
         0xFF,
     ]
+}
+
+#[allow(dead_code)]
+fn message_for_signing(packet: &GossipPacket, proposal: &ProposalTerms) -> Vec<u8> {
+    let packet_bytes = match &packet.data {
+        GossipData::Proposal(terms) => [
+            "Proposal:".as_bytes(),
+            terms.beacon_id.as_bytes(),
+            "\n".as_bytes(),
+            &terms.epoch.to_le_bytes(),
+            "\nLeader:".as_bytes(),
+            terms.leader.address.as_str().as_bytes(),
+            "\n".as_bytes(),
+            &terms.leader.signature,
+        ]
+        .concat(),
+        GossipData::Accept(accept) => [
+            "Accepted:".as_bytes(),
+            accept.acceptor.address.as_str().as_bytes(),
+            "\n".as_bytes(),
+        ]
+        .concat(),
+        GossipData::Reject(reject) => [
+            "Rejected:".as_bytes(),
+            reject.rejector.address.as_str().as_bytes(),
+            "\n".as_bytes(),
+        ]
+        .concat(),
+        GossipData::Abort(abort) => ["Aborted:".as_bytes(), abort.reason.as_bytes()].concat(),
+        GossipData::Execute(execute) => {
+            ["Execute:".as_bytes(), &enc_timestamp(execute.time)].concat()
+        }
+        GossipData::Dkg(..) => "Gossip packet".as_bytes().to_vec(),
+    };
+
+    let mut ret = [
+        "beaconID:".as_bytes(),
+        // Using the `beacon_id` from `packet.metadata` is safe due to earlier authentication, see `MultiBeacon::cmd`.
+        packet.metadata.beacon_id.as_bytes(),
+        "\n".as_bytes(),
+        // Packet validation
+        &packet_bytes,
+        // Proposal validation
+        "Proposal:\n".as_bytes(),
+        proposal.beacon_id.as_bytes(),
+        "\n".as_bytes(),
+        &proposal.epoch.to_le_bytes(),
+        "\nLeader:".as_bytes(),
+        proposal.leader.address.as_str().as_bytes(),
+        "\n".as_bytes(),
+        &proposal.leader.signature,
+        &proposal.threshold.to_le_bytes(),
+        &enc_timestamp(proposal.timeout),
+        &proposal.catchup_period_seconds.get_value().to_le_bytes(),
+        &proposal.beacon_period_seconds.get_value().to_le_bytes(),
+        "\nScheme: ".as_bytes(),
+        proposal.scheme_id.as_bytes(),
+        "\n".as_bytes(),
+        &enc_timestamp(proposal.genesis_time),
+    ]
+    .concat();
+
+    for j in &proposal.joining {
+        ret.extend_from_slice(&enc_participant("\nJoiner:", j))
+    }
+    for r in &proposal.remaining {
+        ret.extend_from_slice(&enc_participant("\nRemainer:", r))
+    }
+    for l in &proposal.leaving {
+        ret.extend_from_slice(&enc_participant("\nLeaver:", l))
+    }
+
+    ret
+}
+
+fn enc_participant(role: &str, identity: &Identity) -> Vec<u8> {
+    [
+        role.as_bytes(),
+        identity.address.as_str().as_bytes(),
+        "\nSig:".as_bytes(),
+        &identity.signature,
+    ]
+    .concat()
 }
 
 #[test]
