2a791db9f777cd7c94b7abf88a4f602360cbdcaa
version513 <88940005+version513@users.noreply.github.com>
2025-06-09 18:39:39 +0800

    feat(e2e): add DKG test and scenario generator (#44)

--- src/test/interop/scripts/execute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source $PWD/src/test/interop/scripts/config.sh
-
-$DRAND dkg execute --id AAA --control $CONTROL
-
-
--- src/test/interop/scripts/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source $PWD/src/test/interop/scripts/config.sh
-
-$DRAND stop --control $CONTROL
-/bin/rm -rf $BASE
-
-
