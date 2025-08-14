ee8b974cf9279485e8ee9bd3ef1ca1c1b91c6138
version513 <seuair@gmail.com>
2025-08-14 16:33:15 +0800

    chain: simplify round ticker

--- src/chain/ticker.rs
+++ src/chain/ticker.rs
@@ -1,95 +1,50 @@
 use super::time;
 use crate::net::utils::Seconds;
-
 use std::time::Duration;
-use std::time::SystemTime;
-use std::time::UNIX_EPOCH;
-use tokio::sync::broadcast;
-use tokio::sync::broadcast::error::SendError;
+use tokio::sync::mpsc;
 
-struct Ticker {
+type Round = u64;
+
+struct RoundTicker {
     period: u32,
     genesis_time: u64,
-    // RoundInfo is required for top chain abstractions (handler, syncer).
-    tx_next_round: broadcast::Sender<RoundInfo>,
-}
-
-#[derive(Clone, Copy)]
-pub struct RoundInfo {
-    pub round: u64,
-    // XXX: remove if no use case.
-    #[allow(dead_code)]
-    pub time: u64,
-}
-
-impl RoundInfo {
-    pub fn now(period: u32, genesis: u64) -> Self {
-        let now = time::time_now().as_secs();
-        let (next_round, next_time) = time::next_round(now, period, genesis);
-
-        Self {
-            round: next_round - 1,
-            time: next_time - period as u64,
-        }
-    }
-}
-
-impl Ticker {
-    /// Broadcasts next round value at next round time to associated receivers.
-    async fn send_next_round(&self) -> Result<(), SendError<RoundInfo>> {
-        let time_now = time::time_now();
-        let (next_round, next_time) =
-            time::next_round(time_now.as_secs(), self.period, self.genesis_time);
-
-        let info = RoundInfo {
-            round: next_round,
-            time: next_time,
-        };
-
-        let sleep_duration = Duration::from_secs(next_time)
-            .checked_sub(time_now)
-            .unwrap_or(Duration::ZERO);
-
-        tokio::time::sleep(sleep_duration).await;
-        let _ = self.tx_next_round.send(info)?;
-
-        Ok(())
-    }
-}
-
-/// Public handle for internal [`Ticker`].
-pub struct RoundTicker {
-    tx_new_round: broadcast::Sender<RoundInfo>,
+    tx_next_round: mpsc::Sender<Round>,
 }
 
 impl RoundTicker {
-    pub fn new() -> Self {
-        let (tx_new_round, _) = broadcast::channel(2);
-        Self { tx_new_round }
+    /// Sends next round value at next round time to associated receiver.
+    async fn send_next_round(&self) -> Result<(), mpsc::error::SendError<Round>> {
+        let utc_now = time::time_now();
+        let (next_round, next_time) =
+            time::next_round(utc_now.as_secs(), self.period, self.genesis_time);
+
+        let sleep_duration = Duration::from_secs(next_time)
+            .checked_sub(utc_now)
+            .unwrap_or(Duration::ZERO);
+
+        tokio::time::sleep(sleep_duration).await;
+        self.tx_next_round.send(next_round).await
     }
+}
 
-    pub fn subscribe(&self) -> broadcast::Receiver<RoundInfo> {
-        self.tx_new_round.subscribe()
-    }
+/// Starts round ticker for given genesis time and period.
+/// Returns associated receiver for new rounds.
+pub fn start_ticker(genesis_time: u64, period: Seconds) -> mpsc::Receiver<Round> {
+    let (tx_next_round, rx_next_round) = mpsc::channel(1);
 
-    /// Starts the ticker for given genesis time and period.
-    pub fn start(self, genesis_time: u64, period: Seconds) -> broadcast::Receiver<RoundInfo> {
-        let rx_round = self.tx_new_round.subscribe();
+    tokio::spawn(async move {
+        let t = RoundTicker {
+            period: period.get_value(),
+            genesis_time,
+            tx_next_round,
+        };
 
-        tokio::spawn(async move {
-            let t = Ticker {
-                period: period.get_value(),
-                genesis_time,
-                tx_next_round: self.tx_new_round,
-            };
-
-            loop {
-                if t.send_next_round().await.is_err() {
-                    break;
-                }
+        loop {
+            if t.send_next_round().await.is_err() {
+                break;
             }
-        });
+        }
+    });
 
-        rx_round
-    }
+    rx_next_round
 }
