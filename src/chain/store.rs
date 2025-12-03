8dba40d59e04de15fc6a2dfae176cb0758bb7d7e
version513 <88940005+version513@users.noreply.github.com>
2025-12-03 16:39:44 +0800

    feat: trimmed database format for chain store (#23)

--- src/chain/store.rs
+++ src/chain/store.rs
@@ -1,3 +1,5 @@
+//! This module implements an actor pattern for a *trimmed* chain store
+//! using [rusqlite] under the hood.
 use crate::{
     core::beacon::BeaconID,
     log::Logger,
@@ -31,6 +33,7 @@ pub struct UnChainedBeacon {
     signature: Vec<u8>,
 }
 
+/// Abstracts chained / unchained beacons into same API.
 #[allow(private_bounds)]
 pub trait BeaconRepr: 'static + Executor + Sized + Send + Sync + Clone {
     fn new(prev: &Self, recovered_sig: Vec<u8>) -> Self;
@@ -40,16 +43,16 @@ pub trait BeaconRepr: 'static + Executor + Sized + Send + Sync + Clone {
     fn from_packet(p: BeaconPacket) -> Self;
     fn from_seed(genesis_seed: Vec<u8>) -> Self;
     fn short_sig(&self) -> String {
-        hex::encode(self.signature().get(..3).expect("value is prechecked"))
+        hex::encode(self.signature().get(..3).unwrap_or_default())
     }
     fn short_prev_sig(&self) -> Option<String> {
         self.prev_signature()
-            .map(|p_sig| hex::encode(p_sig.get(..3).expect("value is prechecked")))
+            .map(|p_sig| hex::encode(p_sig.get(..3).unwrap_or_default()))
     }
 }
 
 impl BeaconRepr for ChainedBeacon {
-    /// WARNING: monotonic round check for new beacon is shifted to caller side.
+    /// WARNING: monotonic round check for previous beacon is shifted to caller side.
     fn new(prev: &Self, new_sig: Vec<u8>) -> Self {
         Self {
             round: prev.round + 1,
@@ -94,7 +97,7 @@ impl BeaconRepr for ChainedBeacon {
 }
 
 impl BeaconRepr for UnChainedBeacon {
-    /// WARNING: monotonic round check for new beacon is shifted to caller side.
+    /// WARNING: monotonic round check for previous beacon is shifted to caller side.
     fn new(prev: &Self, new_sig: Vec<u8>) -> Self {
         Self {
             round: prev.round + 1,
@@ -131,7 +134,21 @@ impl BeaconRepr for UnChainedBeacon {
 
 /// SQL statement executor for [`BeaconRepr`].
 trait Executor: Sized {
-    fn open(path: &Path) -> Result<Connection, Error>;
+    // This is the **trimmed** DB implementation: tables are the same for all schemes.
+    fn open(path: &Path) -> Result<Connection, Error> {
+        let conn = Connection::open(path.join(DB_NAME))?;
+        conn.pragma_update(None, "journal_mode", "WAL")?;
+        conn.pragma_update(None, "synchronous", "NORMAL")?;
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS beacons (
+            round INTEGER PRIMARY KEY,
+            signature BLOB NOT NULL
+        ) WITHOUT ROWID",
+            [],
+        )?;
+
+        Ok(conn)
+    }
     fn get(conn: &Connection, round: u64) -> Result<Self, Error>;
     fn put(self, conn: &mut Connection) -> Result<(), Error>;
     fn last(conn: &Connection) -> Result<Self, Error>;
@@ -143,65 +160,68 @@ trait Executor: Sized {
 }
 
 impl Executor for ChainedBeacon {
-    fn open(path: &Path) -> Result<Connection, Error> {
-        let conn = Connection::open(path.join(DB_NAME))?;
-        conn.pragma_update(None, "journal_mode", "WAL")?;
-        conn.pragma_update(None, "synchronous", "NORMAL")?;
-
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS beacons (
-            round INTEGER PRIMARY KEY,
-            signature BLOB NOT NULL,
-            previous_sig BLOB NOT NULL
-        ) WITHOUT ROWID",
-            [],
-        )?;
-
-        Ok(conn)
-    }
-
     fn get(conn: &Connection, round: u64) -> Result<Self, Error> {
-        let mut stmt = conn.prepare_cached(
-            "SELECT round, signature, previous_sig FROM beacons WHERE round = ?1",
-        )?;
-        stmt.query_row([round], |row| {
-            Ok(Self {
-                round: row.get(0)?,
-                signature: row.get(1)?,
-                previous_signature: row.get(2)?,
+        if round > 0 {
+            let mut stmt = conn.prepare_cached(
+                "SELECT 
+            signature,
+            (SELECT signature FROM beacons WHERE round = b.round - 1)
+         FROM beacons b
+         WHERE round = ?1",
+            )?;
+            stmt.query_row([round], |row| {
+                Ok(Self {
+                    round,
+                    signature: row.get(0)?,
+                    previous_signature: row.get(1)?,
+                })
             })
-        })
+        } else {
+            let mut stmt = conn.prepare_cached(
+                "SELECT 
+            signature
+         FROM beacons b
+         WHERE round = ?1",
+            )?;
+            stmt.query_row([round], |row| {
+                Ok(Self {
+                    round,
+                    signature: row.get(0)?,
+                    previous_signature: vec![],
+                })
+            })
+        }
     }
 
     fn put(self, conn: &mut Connection) -> Result<(), Error> {
         let tr = conn.transaction()?;
-
         {
-            let mut stmt = tr.prepare_cached(
-                "INSERT INTO beacons (round, signature, previous_sig) VALUES (?1, ?2, ?3)",
-            )?;
-            stmt.execute(params![
-                self.round,
-                &self.signature,
-                &self.previous_signature,
-            ])?;
+            let mut stmt =
+                tr.prepare_cached("INSERT INTO beacons (round, signature) VALUES (?1, ?2)")?;
+            stmt.execute(params![self.round, &self.signature,])?;
         }
-
         tr.commit()
     }
 
     fn last(conn: &Connection) -> Result<Self, Error> {
         let mut stmt = conn.prepare_cached(
-            "SELECT round, signature, previous_sig
-         FROM beacons 
+            "SELECT 
+            round, 
+            signature,
+            (SELECT signature FROM beacons WHERE round = b.round - 1)
+         FROM beacons b
          WHERE round = (SELECT MAX(round) FROM beacons)",
         )?;
 
         stmt.query_row([], |row| {
+            let round = row.get(0)?;
+            let signature = row.get(1)?;
+            let previous_signature = row.get(2).unwrap_or_default();
+
             Ok(Self {
-                round: row.get(0)?,
-                signature: row.get(1)?,
-                previous_signature: row.get(2)?,
+                round,
+                signature,
+                previous_signature,
             })
         })
     }
@@ -211,67 +231,61 @@ impl Executor for ChainedBeacon {
         from_round: u64,
         id: &str,
     ) -> Result<Vec<BeaconPacket>, Error> {
+        let mut prev_sig = if from_round == 0 {
+            vec![]
+        } else {
+            let prev_round = from_round - 1;
+            let mut stmt = conn.prepare_cached("SELECT signature FROM beacons WHERE round = ?1")?;
+            stmt.query_row([prev_round], |row| row.get(0))?
+        };
+
         conn.prepare_cached(
-            "SELECT round, signature, previous_sig  
+            "SELECT round, signature 
          FROM beacons 
          WHERE round >= ?1 
          ORDER BY round ASC 
          LIMIT ?2",
         )?
         .query_map([from_round, BATCH_SIZE], |row| {
-            Ok(BeaconPacket {
-                round: row.get(0)?,
-                signature: row.get(1)?,
-                previous_signature: row.get(2)?,
+            let round = row.get(0)?;
+            let signature: Vec<u8> = row.get(1)?;
+
+            let packet = BeaconPacket {
+                round,
+                signature: signature.clone(),
+                previous_signature: std::mem::replace(&mut prev_sig, signature),
                 metadata: Some(Metadata {
                     node_version: None,
                     beacon_id: id.to_string(),
                     chain_hash: vec![],
                 }),
-            })
+            };
+
+            Ok(packet)
         })?
-        .collect::<Result<Vec<BeaconPacket>, _>>()
+        .collect()
     }
 }
 
 impl Executor for UnChainedBeacon {
-    fn open(path: &Path) -> Result<Connection, Error> {
-        let conn = Connection::open(path.join(DB_NAME))?;
-        conn.pragma_update(None, "journal_mode", "WAL")?;
-        conn.pragma_update(None, "synchronous", "NORMAL")?;
-
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS beacons (
-            round INTEGER PRIMARY KEY,
-            signature BLOB NOT NULL
-        ) WITHOUT ROWID",
-            [],
-        )?;
-
-        Ok(conn)
-    }
-
     fn get(conn: &Connection, round: u64) -> Result<Self, Error> {
-        let mut stmt =
-            conn.prepare_cached("SELECT round, signature FROM beacons WHERE round = ?1")?;
+        let mut stmt = conn.prepare_cached("SELECT signature FROM beacons WHERE round = ?1")?;
 
         stmt.query_row([round], |row| {
             Ok(Self {
-                round: row.get(0)?,
-                signature: row.get(1)?,
+                round,
+                signature: row.get(0)?,
             })
         })
     }
 
     fn put(self, conn: &mut Connection) -> Result<(), Error> {
         let tr = conn.transaction()?;
-
         {
             let mut stmt =
                 tr.prepare_cached("INSERT INTO beacons (round, signature) VALUES (?1, ?2)")?;
             stmt.execute(params![self.round, &self.signature])?;
         }
-
         tr.commit()
     }
 
@@ -314,7 +328,7 @@ impl Executor for UnChainedBeacon {
                 }),
             })
         })?
-        .collect::<Result<Vec<BeaconPacket>, _>>()
+        .collect()
     }
 }
 
@@ -552,72 +566,83 @@ fn sync<B: BeaconRepr>(
 mod test {
     use super::*;
 
-    fn generate_unchained(rounds: u64) -> Vec<UnChainedBeacon> {
-        (0..=rounds)
-            .map(|r| UnChainedBeacon {
-                round: r,
-                signature: r.to_be_bytes().into(),
-            })
-            .collect()
+    /// Generates test beacons for 0..=`rounds`.
+    trait BeaconGenerator: BeaconRepr + PartialEq {
+        fn generate(rounds: u64) -> Vec<Self>;
     }
 
-    fn generate_chained(rounds: u64) -> Vec<ChainedBeacon> {
-        (0..=rounds)
-            .map(|r| {
-                if r == 0 {
-                    ChainedBeacon {
-                        round: r,
-                        signature: r.to_be_bytes().into(),
-                        previous_signature: vec![],
-                    }
-                } else {
-                    ChainedBeacon {
-                        round: r,
-                        signature: r.to_be_bytes().into(),
-                        previous_signature: (r - 1).to_be_bytes().into(),
-                    }
-                }
-            })
-            .collect()
+    impl BeaconGenerator for UnChainedBeacon {
+        fn generate(rounds: u64) -> Vec<Self> {
+            (0..=rounds)
+                .map(|r| UnChainedBeacon {
+                    round: r,
+                    signature: r.to_be_bytes().into(),
+                })
+                .collect()
+        }
     }
 
-    #[tokio::test]
-    async fn unchained_store() {
+    impl BeaconGenerator for ChainedBeacon {
+        fn generate(rounds: u64) -> Vec<Self> {
+            (0..=rounds)
+                .map(|r| {
+                    if r == 0 {
+                        ChainedBeacon {
+                            round: r,
+                            signature: r.to_be_bytes().into(),
+                            previous_signature: vec![],
+                        }
+                    } else {
+                        ChainedBeacon {
+                            round: r,
+                            signature: r.to_be_bytes().into(),
+                            previous_signature: (r - 1).to_be_bytes().into(),
+                        }
+                    }
+                })
+                .collect()
+        }
+    }
+
+    async fn test_store<B: BeaconGenerator>() {
         let temp_dir = tempfile::TempDir::new().unwrap();
-        let db_path = temp_dir.path();
         let id = BeaconID::from_static("some_id");
         let log = crate::log::set_id("", id.as_str());
-        let total_beacons = 555;
-        let beacons = generate_unchained(total_beacons);
-        let store = ChainStore::<UnChainedBeacon>::start(db_path.to_path_buf(), id, log.clone())
+
+        // Initialize chain store.
+        let store = ChainStore::<B>::start(temp_dir.path().to_path_buf(), id, log.clone())
             .await
             .unwrap();
 
-        // Add all beacons to the store.
+        // Generate beacons and add into the store.
+        let total_beacons = 555;
+        let beacons = B::generate(total_beacons);
         for b in &beacons {
             store.put(b.clone()).await.unwrap();
         }
-        assert!(store.last().await.unwrap().round == total_beacons);
 
-        // Get all beacons as internal type.
+        // Last beacon should match the expected one.
+        let last = store.last().await.unwrap();
+        let expected = beacons[usize::try_from(total_beacons).unwrap()].clone();
+        assert!(last.round() == expected.round());
+        assert!(last.signature() == expected.signature());
+        assert!(last.prev_signature() == expected.prev_signature());
+
+        // Assert all beacons from the store using [ChainStore::get].
         for i in 0..=total_beacons {
             assert!(store.get(i).await.unwrap() == beacons[usize::try_from(i).unwrap()]);
         }
 
-        // Sync from this store; get all beacons as protobuf packets.
-        let from_round = 1;
-        let mut stream_rx = sync::<UnChainedBeacon>(db_path, from_round, id, log).unwrap();
-
-        // Streamed data should match internal repr.
-        let expected_prev_sig: Vec<u8> = vec![];
-        for i in 1..=total_beacons {
+        // Assert all beacons from the store using [sync].
+        let from_round = 0;
+        let mut stream_rx = sync::<B>(temp_dir.path(), from_round, id, log).unwrap();
+        for i in 0..=total_beacons {
             let packet = stream_rx.recv().await.unwrap().unwrap();
-            assert!(packet.round == i);
             let i_beacon = &beacons[usize::try_from(i).unwrap()];
 
-            assert!(packet.round == i_beacon.round);
-            assert!(packet.signature == i_beacon.signature);
-            assert!(packet.previous_signature == expected_prev_sig);
+            assert!(packet.round == i_beacon.round());
+            assert!(packet.signature == i_beacon.signature());
+            assert!(&packet.previous_signature == i_beacon.prev_signature().unwrap_or_default());
         }
 
         // Next value from stream should be this error.
@@ -631,50 +656,12 @@ mod test {
     }
 
     #[tokio::test]
+    async fn unchained_store() {
+        test_store::<UnChainedBeacon>().await
+    }
+
+    #[tokio::test]
     async fn chained_store() {
-        let temp_dir = tempfile::TempDir::new().unwrap();
-        let db_path = temp_dir.path();
-        let id = BeaconID::from_static("some_id");
-        let log = crate::log::set_id("", id.as_str());
-        let total_beacons = 555;
-        let beacons = generate_chained(total_beacons);
-        let store = ChainStore::<ChainedBeacon>::start(db_path.to_path_buf(), id, log.clone())
-            .await
-            .unwrap();
-
-        // Add all beacons to the store.
-        for b in &beacons {
-            store.put(b.clone()).await.unwrap();
-        }
-        assert!(store.last().await.unwrap().round == total_beacons);
-
-        // Get all beacons as internal type.
-        for i in 0..=total_beacons {
-            assert!(store.get(i).await.unwrap() == beacons[usize::try_from(i).unwrap()]);
-        }
-
-        // Sync from this store; get all beacons as protobuf packets.
-        let from_round = 1;
-        let mut stream_rx = sync::<ChainedBeacon>(db_path, from_round, id, log).unwrap();
-
-        // Streamed data should match internal repr.
-        for i in 1..=total_beacons {
-            let packet = stream_rx.recv().await.unwrap().unwrap();
-            assert!(packet.round == i);
-            let i_beacon = &beacons[usize::try_from(i).unwrap()];
-
-            assert!(packet.round == i_beacon.round);
-            assert!(packet.signature == i_beacon.signature);
-            assert!(packet.previous_signature == i_beacon.previous_signature);
-        }
-
-        // Next value from stream should be the error.
-        let expected_err = format!("no beacons stored above {total_beacons} round");
-        match stream_rx.recv().await.unwrap() {
-            Ok(_) => panic!(),
-            Err(err) => {
-                assert!(err.message() == expected_err);
-            }
-        }
+        test_store::<ChainedBeacon>().await
     }
 }
